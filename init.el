cb0da04d42f051b9e33d6b881527cdfa95d4153d
Han Meh <6310620+HanMeh@users.noreply.github.com>
2025-07-07 19:27:23 +0400

    Update init.el

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
-;;
-;;
-;;
+;;create init.org --> create emacs-init.el
+;;(require 'org)
+;;(org-babel-load-file (expand-file-name "init.org" user-emacs-directory ;replace user-emacs-directory with actual path to your init.org
+;;ensure (package-initialize) is called early-bofore any org-related functions or code blocks in your init.org
 
 
