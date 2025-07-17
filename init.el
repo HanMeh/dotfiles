2d39df8bace35c46e58892e408f9b48ce53fa144
Han Meh <6310620+HanMeh@users.noreply.github.com>
2025-07-18 01:45:43 +0400

    online

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 ;;create init.org --> create emacs-init.el
 ;;(require 'org)
 ;;(org-babel-load-file (expand-file-name "init.org" user-emacs-directory ;replace user-emacs-directory with actual path to your init.org
-;;ensure (package-initialize) is called early-bofore any org-related functions or code blocks in your init.org
+;;ensure (package-initialize) is called early-before any org-related functions or code blocks in your init.org
+;;straight.el > add this "bootstrapping"script
+;; (straight-use-package 'evil)
 
 
