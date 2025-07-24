d244200b8f6751658096b0069676deaa326c453f
Han Meh <6310620+HanMeh@users.noreply.github.com>
2025-07-24 17:47:36 +0400

    Update init.el

--- init.el
+++ init.el
@@ -8,6 +8,17 @@
 ;;(setq inhibit-startup-message t) 
 ;; (setq initial-scratch-message nil)
 ;;package-vc-install function to install packages from github
+;; (require 'package)
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-archives
+;;    (quote
+;;     (("gnu" . "https://elpa.gnu.org/packages/")
+;;      ("melpa" . "https://melpa.org/packages/")))))
+;; (package-initialize)
 
 
 
